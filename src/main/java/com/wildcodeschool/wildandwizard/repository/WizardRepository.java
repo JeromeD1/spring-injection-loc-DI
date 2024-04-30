1780a3d588d3ae6c3bbbe9011a557127c61cf5d2
Jerome Desouches <jerome.desouches@gmail.com>
2024-04-30 16:45:18 +0200

    quest done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+@Repository //must add this annotation to allow this class to be injected
 public class WizardRepository implements WizardDao {
 
     private static List<Wizard> wizards = new ArrayList<>(
